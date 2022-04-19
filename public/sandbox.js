c1107a5e7e70a330856d67e1aab89f8aedce79d7
Kaleb Dykema <dykemakaleb@gmail.com>
2022-04-18 23:02:11 -0500

    Set up starter form event listener

--- public/sandbox.js
+++ public/sandbox.js
@@ -6,3 +6,11 @@ var type = document.querySelector("#type");
 var tofrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
+// Set up form event listener
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value);
+    console.log(tofrom.value);
+    console.log(details.value);
+    console.log(amount.valueAsNumber);
+});
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -6,3 +6,13 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// Set up form event listener
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   console.log(type.value);
+   console.log(tofrom.value);
+   console.log(details.value);
+   console.log(amount.valueAsNumber);
+})
